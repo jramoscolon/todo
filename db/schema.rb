17b325f1110ae4a0a4bef5c792b61dcf57bde0d2
Jose Ramos <jramoscolon@yahoo.com>
2016-06-24 15:26:40 -0400

    added descrition to todo items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160621105013) do
+ActiveRecord::Schema.define(version: 20160624183503) do
 
   create_table "todo_items", force: :cascade do |t|
     t.string   "content"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160621105013) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
     t.datetime "completed_at"
+    t.text     "description"
   end
 
   add_index "todo_items", ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
